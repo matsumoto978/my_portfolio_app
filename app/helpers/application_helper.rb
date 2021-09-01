80459f60896fe5fb13b6d60f2785433b42182789
matsumoto978 <myoshi878@gmail.com>
2021-09-02 00:53:10 +0900

    application_helper.rbの編集

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
   def max_width
-    "mw-xl"
+    if devise_controller?
+      "mw-sm"
+    elsif controller.action_name
+      "mw-xl"
+    else
+      "mw-md"
+    end
   end
 end
